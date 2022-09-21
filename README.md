abc53858311ec77fc02990095744b6eb78e71842
barancet <barcet95@gmail.com>
2022-09-21 16:33:16 -0400

    rearrange and deploy

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # node
 
 Testing backend APIs using node/express and rest client
+
+
+Sites used: 
+
+For APIs:
+https://expressjs.com/en/4x/api.html#req
+
+For Hosting:
+https://fly.io/docs/
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { response } = require("express");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 let notes = [
   {
@@ -23,34 +23,81 @@ let notes = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello !</h1>");
+const requestLogger = (request, response, next) => {
+  console.log("Method", request.method);
+  console.log("Path", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(express.json());
+
+app.use(requestLogger);
+
+app.use(cors());
+
+app.use(express.static("build"));
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello !</h1>");
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => { 
-    console.log(note)
-    return note.id === id});
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(400).send("ID Does not exist");
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
-  //console.log(note, "note");
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  console.log(note);
+  response.json(note);
 });
 
-app.delete("/api/notes/:id", (req, res) => {
+app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`listening on port ${PORT}`);
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note =>  note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(400).send("ID Does not exist");
+  }
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
